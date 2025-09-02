d96a3ec763c44b371503f9eece4963e07a1a001f
Humza Syed <hsyed@my.wctc.edu>
2025-09-02 14:07:39 -0500

    [1.06] Write data to file

--- Program.cs
+++ Program.cs
@@ -22,6 +22,8 @@ if (resp == "1")
     DateTime dataDate = dataEndDate.AddDays(-(weeks * 7));
     // random number generator
     Random rnd = new();
+    // create file
+    StreamWriter sw = new("dataDate.txt");
 
     // loop for the desired # of weeks
     while (dataDate < dataEndDate)
@@ -34,10 +36,11 @@ if (resp == "1")
             hours[i] = rnd.Next(4, 13);
         }
         //M/d/yyyy,#|#|#|#|#|#|#
-        Console.WriteLine($"{dataDate:M/d/yy},{string.Join("|", hours)}");
+        sw.WriteLine($"{dataDate:M/d/yy},{string.Join("|", hours)}");
         // add 1 week to date
         dataDate = dataDate.AddDays(7);
     }
+    sw.Close();
 }
 else if (resp == "2")
 {
