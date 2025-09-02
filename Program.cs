5ad17c475c250866a63efa68ba1491b897c26597
Humza Syed <hsyed@my.wctc.edu>
2025-09-02 13:55:24 -0500

    [1.04] Determine end data

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿Console.WriteLine("Enter 1 to create data file.");
+﻿using System.Data;
+// ask for input
+Console.WriteLine("Enter 1 to create data file.");
 Console.WriteLine("Enter 2 to parse data file.");
 Console.WriteLine("Enter anything else to quit");
 
@@ -11,6 +13,13 @@ if (resp == "1")
     Console.WriteLine("How many weeks of data is needed?");
     // input the response (convert to int)
     int weeks = Convert.ToInt32(Console.ReadLine());
+    // determine start and end date
+    DateTime today = DateTime.Now;
+    // we want full weeks sunday - saturday
+    DateTime dataEndDate = today.AddDays(-(int)today.DayOfWeek);
+    // subtract # of weeks from endDate to get startDate
+    DateTime dataDate = dataEndDate.AddDays(-(weeks * 7));
+    Console.WriteLine(dataDate);
 }
 else if (resp == "2")
 {
