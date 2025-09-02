f86995fd266df893e201f49198f1b67e08edfe09
Humza Syed <hsyed@my.wctc.edu>
2025-09-02 13:34:56 -0500

    [1.02] Create user options

--- Program.cs
+++ Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿Console.WriteLine("Enter 1 to create data file.");
+Console.WriteLine("Enter 2 to parse data file.");
+Console.WriteLine("Enter anything else to quit");
+
+string? resp = Console.ReadLine();
+
+if (resp == "1")
+{
+    //TODO: create data file
+}
+else if (resp == "2")
+{
+    //TODO: parse data file
+}
